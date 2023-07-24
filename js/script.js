d4800c97f3f324684dc17ac13cec67dacb5a15b7
Asif Imtiaz Shaafi <shaafi.20994@gmail.com>
2023-07-24 01:01:42 -0500

    Add alert for comment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
+$('#comment_submit_btn').on('click', function () {
+  console.log('clicked');
+  $('#hiddenAlertContainer').css('display', 'block');
+
+  setTimeout(function () {
+    $('#hiddenAlertContainer').css('display', 'none');
+  }, 3000);
+});
+
 function navFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
